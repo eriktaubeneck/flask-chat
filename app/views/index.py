a38b8136aa2b9879be1bc642d1648c4ab504d80f
Erik Taubeneck <erik.taubeneck@gmail.com>
2014-11-13 13:29:48 -0500

    refactor index blueprint to ditch class model

--- app/views/index.py
+++ app/views/index.py
@@ -8,46 +8,34 @@ from app.forms import RegisterForm
 index_bp = Blueprint('index', __name__, url_prefix='/')
 
 
-class IndexView:
-    @classmethod
-    def index(cls, **kwargs):
-        return render_template('index.html', **kwargs)
+@index_bp.route('/', methods=('GET',))
+def index(**kwargs):
+    return render_template('index.html', **kwargs)
 
-    @classmethod
-    def register(cls, login_failed=False, **kwargs):
-        if current_user.is_authenticated():
-            return redirect(request.referrer or '/')
 
-        form = RegisterForm()
+@index_bp.route('/register', methods=('GET', 'POST'))
+def register(login_failed=False, **kwargs):
+    if current_user.is_authenticated():
+        return redirect(request.referrer or '/')
 
-        ds = get_extension('security', app=current_app).datastore
-        if form.validate_on_submit():
-            user = ds.create_user(
-                fullname=form.fullname.data,
-                email=form.email.data,
-                password=form.password.data)
-            ds.commit()
+    form = RegisterForm()
 
-            login_user(user)
-            ds.commit()
-            flash('Account created successfully', 'info')
-            return cls.index(show_modal='profile')
+    ds = get_extension('security', app=current_app).datastore
+    if form.validate_on_submit():
+        user = ds.create_user(
+            fullname=form.fullname.data,
+            email=form.email.data,
+            password=form.password.data)
+        ds.commit()
 
-        if form.errors:
-            show_modal = 'login-signup'
-        else:
-            show_modal = None
+        login_user(user)
+        ds.commit()
+        flash('Account created successfully', 'info')
+        return index(show_modal='profile')
 
-        return cls.index(
-            register_form=form,
-            show_modal=show_modal)
+    if form.errors:
+        show_modal = 'login-signup'
+    else:
+        show_modal = None
 
-    @classmethod
-    def register_routes(cls, index_bp):
-        index_bp.add_url_rule(
-            '', 'index', view_func=cls.index, methods=["GET"])
-        index_bp.add_url_rule(
-            'register', 'register', view_func=cls.register,
-            methods=['POST', 'GET'])
-
-IndexView.register_routes(index_bp)
+    return index(register_form=form, show_modal=show_modal)
