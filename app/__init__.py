10cff5f66ebbe7cdbaee1d51196e057197054653
Erik Taubeneck <erik.taubeneck@gmail.com>
2014-11-23 14:38:17 -0500

    remove unused user model, security, and admin

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,5 @@
 from app.chat import Chat
-from flask.ext.security import Security
 from flask_sockets import Sockets
 
-security = Security()
 sockets = Sockets()
 chat = Chat()
--- manage.py
+++ manage.py
@@ -1,51 +1,9 @@
-from datetime import datetime
 from flask.ext.script import Manager
-from flask.ext.security.utils import encrypt_password
-from foundry import SQLAlchemyFoundry, Mold
 from app.factory import create_app
-from app.models import db
-from app.models.user import User, Role
 
 app = create_app()
 
 manager = Manager(app)
-
-user_converters = {
-    'confirmed_at': lambda s: datetime.strptime(s, '%Y-%m-%d'),
-    'password': lambda s: encrypt_password(s),
-}
-
-foundry = SQLAlchemyFoundry(
-    db.session,
-    [('roles.yml', Role),
-     Mold('users.yml', User, str_converters=user_converters), ],
-    path='foundry_data',
-)
-
-
-@manager.shell
-def _make_context():
-    return {'db': db,
-            'fixtures': foundry}
-
-
-@manager.command
-def drop_and_create_db():
-    db.drop_all()
-    db.create_all()
-
-
-@manager.command
-def populate_db():
-    foundry.load()
-
-
-@manager.command
-def update_demo_password():
-    user, password = foundry['zoidberg'], app.config['SECRET_PASSWORD']
-    user.password = encrypt_password(password)
-    db.session.add(user)
-    db.session.commit()
 
 
 if __name__ == '__main__':
